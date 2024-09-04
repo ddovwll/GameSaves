2d0c513be8073423674603fbc09dd2f8f3a11d80
ddovwll <robertinkashenoid@gmail.com>
2024-09-04 21:11:06 +0300

    сделана логика для пользователя

---
+++ IdentityService.Core/Contracts/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using IdentityService.Core.Models;
+
+namespace IdentityService.Core.Contracts;
+
+public interface IUserService
+{
+    Task<User> AddAsync(User user);
+    Task<User> GetByIdAsync(Guid userId);
+    Task<User> GetByNameAsync(string userName);
+    Task<User> UpdateNameAsync(Guid userId, string userName);
+    Task<User> UpdatePasswordAsync(Guid userId, string password);
+    Task DeleteAsync(Guid userId);
+    Task<bool> AuthenticateAsync(string username, string password);
+}
---
+++ IdentityService.Core/Exceptions/NotFoundException.cs
@@ -0,0 +1,3 @@
+﻿namespace IdentityService.Core.Exceptions;
+
+public class NotFoundException(string message) : Exception(message);
