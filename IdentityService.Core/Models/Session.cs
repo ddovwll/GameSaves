b5722447a39b52e5988440d7494d08928e92cd3b
ddovwll <robertinkashenoid@gmail.com>
2024-09-04 19:42:40 +0300

    добавлены модели

---
+++ IdentityService.Core/Models/Session.cs
@@ -0,0 +1,11 @@
+﻿namespace IdentityService.Core.Models;
+
+public class Session
+{
+    public Guid Id { get; init; }
+    public User User { get; init; }
+    public Guid UserId { get; init; }
+    public DateTime CreatedOn { get; init; }
+    public const int ExpiresOnDays = 60;
+    public string FingerPrint { get; init; }
+}
---
+++ IdentityService.Core/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace IdentityService.Core.Models;
+
+public class User
+{
+    public Guid Id { get; init; }
+    public string Name { get; init; }
+    public string Password { get; init; }
+    public string Salt { get; init; }
+}
