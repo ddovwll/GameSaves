7adfbfddead34b39dc1ccabceb2fbdb1231aca5e
ddovwll <robertinkashenoid@gmail.com>
2024-09-05 21:33:35 +0300

    добавлена модель сохранения и репозиторий

---
+++ SavesService.Core/Models/GameSave.cs
@@ -0,0 +1,13 @@
+﻿namespace SavesService.Core.Models;
+
+public class GameSave
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public string Name { get; set; }
+    public string Path { get; set; }
+    public string Game { get; set; }
+    public string GameVersion { get; set; }
+    public DateTime Date { get; set; }
+    public bool Shared { get; set; }
+}
